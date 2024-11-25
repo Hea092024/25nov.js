55fab3e61a33a328cf1f12146d8dd3bb3f9501da
Hea092024 <Hea092024@gmail.com>
2024-11-25 09:59:35 +0100

    updated script

---
+++ script.js
@@ -0,0 +1,11 @@
+//sets up constants fro m the DOM
+const setup = document.getElementById("setup");
+const punchline = document.getElementById("punchline");
+const randButton = document.getElementById("rand-button");
+const dadButton = document.getElementById("dad-button");
+const chuCkButton = document.getElementById("chuck-button");
+
+//sets up API urls
+const randUrl = "https://official-joke-api.appspot.com/random_joke";
+const dadUrl = "https://icanhazdadjoke.com/";
+const chuckUrl = "https://api.chucknorris.io/jokes/random";
